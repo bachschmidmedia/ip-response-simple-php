7c54d915fd50136d622624a3b00528b71bdbcc51
Show own IP. With CURL or Ping <myip_io7@note.io7.pw>
2022-02-27 14:10:37 +0100

    initial commit

---
+++ .index.old.php
@@ -0,0 +1,12 @@
+<?php
+$ip = 'Could\'nt get IP';
+if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+    $ip = $_SERVER['HTTP_CLIENT_IP'];
+} elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+    $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+} else {
+    $ip = $_SERVER['REMOTE_ADDR'];
+}
+
+echo "{$ip}\r\n";
+return;
---
+++ index.php
@@ -0,0 +1,118 @@
+<?php
+
+class RemoteAddress
+{
+    /**
+     * Whether to use proxy addresses or not.
+     *
+     * As default this setting is disabled - IP address is mostly needed to increase
+     * security. HTTP_* are not reliable since can easily be spoofed. It can be enabled
+     * just for more flexibility, but if user uses proxy to connect to trusted services
+     * it's his/her own risk, only reliable field for IP address is $_SERVER['REMOTE_ADDR'].
+     *
+     * @var bool
+     */
+    protected $useProxy = false;
+
+    /**
+     * List of trusted proxy IP addresses
+     *
+     * @var array
+     */
+    protected $trustedProxies = [];
+
+    /**
+     * HTTP header to introspect for proxies
+     *
+     * @var string
+     */
+    protected $proxyHeader = 'HTTP_X_FORWARDED_FOR';
+
+    /**
+     * Returns client IP address.
+     *
+     * @return string IP address.
+     */
+    public function getIpAddress()
+    {
+        $ip = $this->getIpAddressFromProxy();
+        if ($ip) {
+            return $ip;
+        }
+
+	// Direct IP address.
+        if (isset($_SERVER['REMOTE_ADDR'])) {
+            return $_SERVER['REMOTE_ADDR'];
+        }
+
+        return '';
+    }
+
+    /**
+     * Attempt to get the IP address for a proxied client
+     *
+     * @return false|string
+     */
+    protected function getIpAddressFromProxy()
+    {
+        if (
+            !$this->useProxy ||
+            (isset($_SERVER['REMOTE_ADDR']) && !in_array($_SERVER['REMOTE_ADDR'], $this->trustedProxies))
+	) {
+            return false;
+        }
+
+        $header = $this->proxyHeader;
+        if (!isset($_SERVER[$header]) || empty($_SERVER[$header])) {
+            return false;
+        }
+
+        // Extract IPs
+        $ips = explode(',', $_SERVER[$header]);
+        // trim, so we can compare against trusted proxies properly
+        $ips = array_map('trim', $ips);
+        // remove trusted proxy IPs
+        $ips = array_diff($ips, $this->trustedProxies);
+
+        // Any left?
+        if (empty($ips)) {
+            return false;
+        }
+
+        // Since we've removed any known, trusted proxy servers, the right-most
+        // address represents the first IP we do not know about -- i.e., we do
+        // not know if it is a proxy server, or a client. As such, we treat it
+        // as the originating IP.
+        // @see http://en.wikipedia.org/wiki/X-Forwarded-For
+        $ip = array_pop($ips);
+        return $ip;
+    }
+
+    // [...]
+}
+
+$remote = new RemoteAddress();
+
+if(str_starts_with($_SERVER['HTTP_USER_AGENT'], 'curl')) {
+	echo "{$remote->getIpAddress()}\r\n";
+} else {
+
+echo <<<END
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>{$remote->getIpAddress()}</title>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" href="style.css">
+</head>
+<body>
+	<h1>{$remote->getIpAddress()}</h1>
+</body>
+</html>
+END;
+
+}
+
+
